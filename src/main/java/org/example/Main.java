ad9b39cd4790441ba859e8ec193d77154d7c0dd6
Framsix <kendis200060@gmail.com>
2023-04-15 17:11:05 +0300

    1.1.4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.nio.file.*;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "c:\\windows\\projects\\note.txt";
+        Path path = Path.of(str).toAbsolutePath();
+        System.out.println(path);
     }
+
+}
+
+
+class test {
+
+
+
 }
